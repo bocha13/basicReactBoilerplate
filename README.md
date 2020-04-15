db0bd6008c956ded592008904d8241873a51955c
Ignacio Bochatay <ignacio.bochatay@learnlight.com>
2020-04-15 19:58:33 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Basic React boilerplate
+
+This project is a basic setup for React with Webpack4 and Babel.
+
+## To update the package.json to latest versions do:
+
+```shell
+  yarn upgrade <package names> --latest
+```
---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localsConvention: "camelCase",
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      favicon: "public/favicon.ico",
+    }),
+  ],
+};
